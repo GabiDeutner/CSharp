831d84759b2359b546689ef1273e71eb68807388
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2022-11-13 02:05:15 -0300

    Create somasimples.cs

---
+++ URI_iniciante/somasimples.cs
@@ -0,0 +1,18 @@
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A, B, SOMA;
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+
+    }
+
+}
