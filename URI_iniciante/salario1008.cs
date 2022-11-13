c8d8e4b6b6d5a53061053b4d449e73d56abc468b
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2022-11-13 16:29:43 -0300

    Update salario1008.cs

--- URI_iniciante/salario1008.cs
+++ URI_iniciante/salario1008.cs
@@ -1,4 +1,4 @@
-/*
+/* URI 1008
 Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
 
 Entrada
