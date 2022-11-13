f18352ab9fac01945e687f9401dd665b763fca22
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2022-11-13 15:05:56 -0300

    Create produtosimples.cs

---
+++ URI_iniciante/produtosimples.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Globalization;
+
+namespace uri1004 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A, B, PROD;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            PROD = A * B;
+            Console.WriteLine("PROD = " + PROD);
+
+            
+        }
+    }
+}
